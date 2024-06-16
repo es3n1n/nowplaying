e2e5693372bc1dfc8a79875d658af66d3505d63e
Arsenii es3n1n <me@es3n.in>
2024-06-17 00:44:50 +0200

    refactor: remove unused frontend stuff

--- nowplaying/__init__.py
+++ nowplaying/__init__.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from starlette.staticfiles import StaticFiles
 
 from .bot import import_bot_handlers
 from .routes.ext import router as ext_router
@@ -15,7 +14,7 @@ app.include_router(ext_router)
 import_bot_handlers()
 
 # todo: move to a router once it would be possible (not implemented in fastapi atm)
-ym_frontend_path = ROOT_DIR / 'frontend' / 'ym' / 'web-app' / 'build'
-if not ym_frontend_path.exists():
-    raise ValueError('no ym frontend')
-app.mount('/ym', StaticFiles(directory=ym_frontend_path, html=True))
+# ym_frontend_path = ROOT_DIR / 'frontend' / 'ym' / 'web-app' / 'build'
+# if not ym_frontend_path.exists():
+#     raise ValueError('no ym frontend')
+# app.mount('/ym', StaticFiles(directory=ym_frontend_path, html=True))
