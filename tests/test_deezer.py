73f0a681d635949b38bac836750eccc5808eeaf3
Arsenii es3n1n <me@es3n.in>
2024-07-15 19:59:56 +0200

    test: add deezer jp search tests

--- tests/test_deezer.py
+++ tests/test_deezer.py
@@ -15,3 +15,23 @@ async def test_deezer_search() -> None:
     assert results[0].artist == 'MK (JPN)'
     assert results[0].artist_id == '10934636'
     assert results[0].url == 'https://www.deezer.com/track/1265359402'
+
+
+@mark.asyncio
+async def test_deezer_search_jp_title() -> None:
+    results_jp = await search_tracks('Homecomings - ラプス')
+    results_en = await search_tracks('Homecomings - Lapse')
+
+    assert results_en == results_jp
+    assert results_jp[0].title == 'Lapse'
+    assert results_jp[0].artist == 'Homecomings'
+
+
+@mark.asyncio
+async def test_deezer_search_jp_artist() -> None:
+    results_jp = await search_tracks('kessoku band - あのバンド - That band')
+    results_en = await search_tracks('結束バンド - あのバンド')
+
+    assert results_en[0] == results_jp[0]
+    assert results_jp[0].title == 'That band'
+    assert results_jp[0].artist == 'kessoku band'
