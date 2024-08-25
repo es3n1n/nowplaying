cb45e6b3e05b9e8749284d59e191a69a8d5f4d3d
Arsenii es3n1n <me@es3n.in>
2024-08-25 05:04:07 +0200

    refactor(ynison): migrate to aiohttp

--- nowplaying/external/yaynison.py
+++ nowplaying/external/yaynison.py
@@ -1,19 +1,9 @@
 # Ported from https://github.com/bulatorr/go-yaynison/
-from collections.abc import Sequence
 from dataclasses import dataclass
+from uuid import uuid4
 
 import orjson
-from websockets import WebSocketClientProtocol, connect, headers
-from websockets.exceptions import WebSocketException
-from websockets.typing import Subprotocol
-
-
-def do_nothing(*_: Sequence[Subprotocol]) -> None:
-    return None
-
-
-# Workaround for ticket's json subprotocol
-headers.validate_subprotocols.__code__ = do_nothing.__code__
+from aiohttp import ClientError, ClientSession, ClientWebSocketResponse
 
 
 class YaynisonError(Exception):
@@ -32,10 +22,8 @@ class Yaynison:
     def __init__(self, token: str) -> None:
         self._token = token
         self._device = 'playinnowbot'
-        self._headers = [
-            ('Origin', 'https://music.yandex.ru'),
-            ('Authorization', f'OAuth {self._token}'),
-        ]
+        self._queue_version = 9021243204784341000
+        self._status_version = 8321822175199937000
 
         self._connection_uri: str | None = None
         self._redirect_ticket: str | None = None
@@ -54,7 +42,7 @@ class Yaynison:
                             'entity_context': 'BASED_ON_ENTITY_BY_DEFAULT',
                             'version': {
                                 'device_id': self._device,
-                                'version': 9021243204784341000,
+                                'version': self._queue_version,
                                 'timestamp_ms': 0,
                             },
                             'from_optional': '',
@@ -66,7 +54,7 @@ class Yaynison:
                             'progress_ms': 0,
                             'version': {
                                 'device_id': self._device,
-                                'version': 8321822175199937000,
+                                'version': self._status_version,
                                 'timestamp_ms': 0,
                             },
                         },
@@ -80,7 +68,7 @@ class Yaynison:
                         'info': {
                             'device_id': self._device,
                             'type': 'WEB',
-                            'title': 'playinnowbot',
+                            'title': self._device,
                             'app_name': 'Chrome',
                         },
                         'volume_info': {'volume': 0},
@@ -88,37 +76,52 @@ class Yaynison:
                     },
                     'is_currently_active': False,
                 },
-                'rid': 'ac281c26-a047-4419-ad00-e4fbfda1cba3',
+                'rid': str(uuid4()),
                 'player_action_timestamp_ms': 0,
                 'activity_interception_type': 'DO_NOT_INTERCEPT_BY_DEFAULT',
             }
         ).decode()
 
-        self._conn: WebSocketClientProtocol | None = None
-
-    async def startup(self) -> None:
+    async def one_shot_playable_items(self) -> list[YanisonPlayableItem]:
         await self.request_ticket()
-        await self.connect()
+        return await self.get_playable_items()
 
     async def request_ticket(self) -> None:
-        try:
-            conn = await connect(
+        async with (
+            ClientSession() as session,
+            session.ws_connect(
                 'wss://ynison.music.yandex.ru/redirector.YnisonRedirectService/GetRedirectToYnison',
-                extra_headers=self._headers,
-                subprotocols=self._subprotocols,  # type: ignore[arg-type]
-                compression=None,
-            )
-        except WebSocketException as err:
-            msg = 'unable to connect to ynison redirect'
-            raise YaynisonError(msg) from err
+                headers=self._headers,
+            ) as ws,
+        ):
+            try:
+                await self._obtain_ticket(ws)
+            except ClientError as err:
+                msg = 'unable to obtain ticket'
+                raise YaynisonError(msg) from err
 
-        try:
-            message = orjson.loads(await conn.recv())
-        except WebSocketException as err:
-            msg = 'unable to get the ticket'
-            raise YaynisonError(msg) from err
+    async def get_playable_items(self) -> list[YanisonPlayableItem]:
+        if self._connection_uri is None:
+            msg = 'no ticket'
+            raise ValueError(msg)
 
-        await conn.close()
+        async with (
+            ClientSession() as session,
+            session.ws_connect(
+                self._connection_uri,
+                headers=self._headers,
+            ) as ws,
+        ):
+            try:
+                await ws.send_str(self._config_message)
+                return await self._get_playable_items(ws)
+            except ClientError as err:
+                msg = 'unable to obtain playable items'
+                raise YaynisonError(msg) from err
+
+    async def _obtain_ticket(self, ws: ClientWebSocketResponse) -> None:
+        message = orjson.loads(await ws.receive_str())
+
         if 'error' in message:
             msg = 'got an error in ticket logic'
             raise YaynisonError(msg)
@@ -127,49 +130,11 @@ class Yaynison:
         self._redirect_ticket = message['redirect_ticket']
         self._session_id = message['session_id']
 
-    async def disconnect(self) -> None:
-        if self._conn is None:
-            return
-
-        await self._conn.close()
-
-    async def connect(self) -> None:
-        if self._connection_uri is None:
-            await self.request_ticket()
-
-            if self._connection_uri is None:
-                msg = 'no ticket'
-                raise ValueError(msg)
-
-        try:
-            self._conn = await connect(
-                self._connection_uri,
-                extra_headers=self._headers,
-                subprotocols=self._subprotocols,  # type: ignore[arg-type]
-                compression=None,
-            )
-        except WebSocketException as err:
-            msg = 'unable to connect to yaynison'
-            raise YaynisonError(msg) from err
-
-        try:
-            await self._conn.send(self._config_message)
-        except WebSocketException as err:
-            await self.disconnect()
-            msg = 'unable to send message to yaynison'
-            raise YaynisonError(msg) from err
-
-    async def get_playable_items(self) -> list[YanisonPlayableItem]:
-        if self._conn is None:
-            await self.connect()
-
-            if self._conn is None:
-                msg = 'conn is none'
-                raise ValueError(msg)
-
+    @staticmethod
+    async def _get_playable_items(ws: ClientWebSocketResponse) -> list[YanisonPlayableItem]:
         playable = []
 
-        message = orjson.loads(await self._conn.recv())
+        message = orjson.loads(await ws.receive_str())
         player_queue = message.get('player_state', {}).get('player_queue', {})
         current_index = player_queue.get('current_playable_index', 0)
         playable_list = player_queue.get('playable_list', [])
@@ -190,12 +155,6 @@ class Yaynison:
 
         return playable
 
-    async def one_shot_playable_items(self) -> list[YanisonPlayableItem]:
-        await self.startup()
-        playable_items = await self.get_playable_items()
-        await self.disconnect()
-        return playable_items
-
     @property
     def _ticket_subprotocol(self) -> str:
         subprotocol_data = {
@@ -212,5 +171,9 @@ class Yaynison:
         return orjson.dumps(subprotocol_data).decode()
 
     @property
-    def _subprotocols(self) -> list[str]:
-        return ['Bearer', 'v2', self._ticket_subprotocol]
+    def _headers(self) -> dict[str, str]:
+        return {
+            'Origin': 'https://music.yandex.ru',
+            'Authorization': f'OAuth {self._token}',
+            'Sec-Websocket-Protocol': f'Bearer, v2, {self._ticket_subprotocol}',
+        }
