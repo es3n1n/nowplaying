9e276044f499082be83f3c2cbb289ce82a9160f0
Arsenii es3n1n <me@es3n.in>
2024-08-29 00:15:24 +0200

    fix(caching): catch internal server errors on file uploading

--- nowplaying/bot/caching.py
+++ nowplaying/bot/caching.py
@@ -1,10 +1,11 @@
-from aiogram.exceptions import AiogramError, TelegramAPIError
-from aiogram.types import BufferedInputFile, URLInputFile, User
+from aiogram.exceptions import AiogramError, TelegramAPIError, TelegramBadRequest
+from aiogram.types import BufferedInputFile, Message, URLInputFile, User
 
 from nowplaying.bot.bot import bot
 from nowplaying.core.config import config
 from nowplaying.core.database import db
 from nowplaying.models.track import Track
+from nowplaying.util.retries import retry
 
 
 async def get_cached_file_id(uri: str) -> str | None:
@@ -37,15 +38,25 @@ async def cache_file(
     caption += f'#uid_{user.id} #u_{user.username!s} {user.full_name}\n'
     caption += f'{track.artist} - {track.name}'
 
-    sent = await bot.send_audio(
-        config.BOT_CACHE_CHAT_ID,
-        BufferedInputFile(file=file_data, filename=f'{track.artist} - {track.name}.mp3'),
-        caption=caption,
-        performer=track.artist,
-        title=track.name,
-        thumbnail=thumbnail,
-    )
-    if sent.audio is None:
+    sent: Message | None = None
+
+    async for _ in retry(3):
+        try:
+            sent = await bot.send_audio(
+                config.BOT_CACHE_CHAT_ID,
+                BufferedInputFile(file=file_data, filename=f'{track.artist} - {track.name}.mp3'),
+                caption=caption,
+                performer=track.artist,
+                title=track.name,
+                thumbnail=thumbnail,
+            )
+        except TelegramBadRequest as exc:
+            if 'internal Server Error during file upload' in exc.message:
+                continue
+
+            raise
+
+    if not sent or not sent.audio:
         msg = 'Audio is none'
         raise ValueError(msg)
 
