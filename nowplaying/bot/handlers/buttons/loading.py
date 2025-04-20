d6ea1b4b1a28da81332a5ea01df72a47f207542f
es3n1n <me@es3n.in>
2025-04-20 20:55:54 +0200

    fix(caching): ignore old loading buttons

--- nowplaying/bot/handlers/buttons/loading.py
+++ nowplaying/bot/handlers/buttons/loading.py
@@ -11,8 +11,18 @@ async def handle_loading_button(query: CallbackQuery) -> None:
         msg = 'Query data is None'
         raise ValueError(msg)
 
-    _, track_uri = extract_from_query(query.data, arguments_count=2)
+    query_args = extract_from_query(query.data, arguments_count=2)
+    if len(query_args) != 2:  # noqa: PLR2004
+        await bot.answer_callback_query(
+            query.id,
+            text='It seems like you are trying to click on a very old button.'
+            '\nUnfortunately, these (old) buttons are not supported anymore.'
+            '\nYou can generate a new one by sending fresh-new track from inline menu.',
+            show_alert=True,
+        )
+        return
 
+    _, track_uri = query_args
     if DOWNLOADING_LOCKS.is_locked(track_uri):
         await bot.answer_callback_query(query.id, text='Already downloading the audio, please wait.')
         return
